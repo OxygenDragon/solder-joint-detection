387f48f523654603ead7426a98efce80834631e3
jumha <e24076750@gs.ncku.edu.tw>
2021-07-17 23:50:52 +0800

    haha haha hates tensorflow

--- utils/detection.py
+++ utils/detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import tensorflow as tf
 
 # get the bounding box of detecting image and show
 # arguments:
@@ -8,8 +9,157 @@ import cv2
 # return: none
 
 
+class_name = [
+  "insufficient",
+  "short   ",
+  "too much"
+]
+out_zero_point = 6
+out_scale = 0.01769256219267845
+
 def get_bounding_boxes(img, predictions, img_number):
-    cv2.imwrite('himax_pictures/himax_3_{}.jpg'.format(img_number + 34), img)
+    predictions = np.float32((predictions  - out_zero_point)* out_scale)
+    predictions = _detection_layer(predictions, num_classes=3, anchors=[(10, 14),  (23, 27),  (37, 58),] , img_size=[384,384], data_format='NHWC')
+    boxes, classes, scores = handle_predictions(predictions, confidence=0.1, iou_threshold=0.5)
+    draw_boxes(boxes, classes, scores, img, class_name)
     cv2.imshow('Detecting result', img)
     cv2.waitKey(1)
-    return
+
+def handle_predictions(predictions, confidence=0.6, iou_threshold=0.5):
+    boxes = predictions[:, :, :4]
+    box_confidences = np.expand_dims(predictions[:, :, 4], -1) 
+    box_confidences = np.maximum(box_confidences, 0)
+    box_confidences = np.minimum(box_confidences, 1)
+
+    box_class_probs = predictions[:, :, 5:] 
+    box_class_probs = np.maximum(box_class_probs, 0)
+    box_class_probs = np.minimum(box_class_probs, 1)
+
+    box_scores = list(box_confidences * box_class_probs)    
+    box_class_scores = np.max(box_scores, axis=-1)
+    box_classes = np.argmax(box_scores, axis=-1)
+    pos = np.where(box_class_scores >= confidence)
+    boxes = np.array(boxes)
+    boxes = boxes[pos]
+    classes = box_classes[pos]
+    scores = box_class_scores[pos]
+    n_boxes, n_classes, n_scores = nms_boxes(boxes, classes, scores, iou_threshold)
+
+    if n_boxes:
+        boxes = np.concatenate(n_boxes)
+        classes = np.concatenate(n_classes)
+        scores = np.concatenate(n_scores)
+
+        return boxes, classes, scores
+
+    else:
+        return None, None, None
+
+
+def nms_boxes(boxes, classes, scores, iou_threshold):
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+
+        x = b[:, 0]
+        y = b[:, 1]
+        w = b[:, 2]
+        h = b[:, 3]
+
+        areas = w * h
+        order = s.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+
+            xx1 = np.maximum(x[i], x[order[1:]])
+            yy1 = np.maximum(y[i], y[order[1:]])
+            xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
+            yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
+
+            w1 = np.maximum(0.0, xx2 - xx1 + 1)
+            h1 = np.maximum(0.0, yy2 - yy1 + 1)
+
+            inter = w1 * h1
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
+            inds = np.where(ovr <= iou_threshold)[0]
+            order = order[inds + 1]
+
+        keep = np.array(keep)
+
+        nboxes.append(b[keep])
+        nclasses.append(c[keep])
+        nscores.append(s[keep])
+    return nboxes, nclasses, nscores
+
+
+def draw_boxes(boxes, classes, scores, img, class_name):
+    if boxes is not None:
+        for box, score, cls in zip(boxes, scores, classes):
+            left = (int)(box[0]- box[2]/2)
+            top = (int)(box[1] - box[3]/2 )
+            right = (int)(box[0]  + box[2]/2 )
+            bottom = (int)(box[1]  + box[3]/2 )
+            color = (0,255,0)
+            thick = 2
+            if top < 30:
+                cv2.putText(img,'{}: {:.2f}%'.format(class_name[cls], score * 100),(left, bottom + 20),0,thick/3,color)
+            else:
+                cv2.putText(img,'{}: {:.2f}%'.format(class_name[cls], score * 100),(left, top - 10),0,thick/3,color)
+            cv2.rectangle(img, (left, top), (right, bottom), color, thick)
+
+def _get_size(shape, data_format):
+    if len(shape) == 4:
+        shape = shape[1:]
+    return shape[1:3] if data_format == 'NCHW' else shape[0:2]
+
+def _detection_layer(predictions, num_classes, anchors, img_size, data_format):
+    num_anchors = len(anchors)
+
+    shape = np.array(predictions).shape
+    grid_size = _get_size(shape, data_format)
+    dim = grid_size[0] * grid_size[1]
+    bbox_attrs = 5 + num_classes
+
+    if data_format == 'NCHW':
+        predictions = tf.reshape(predictions, [-1, num_anchors * bbox_attrs, dim])
+        predictions = tf.transpose(predictions, [0, 2, 1])
+
+    predictions = tf.reshape(predictions, [-1, num_anchors * dim, bbox_attrs])
+
+    stride = (img_size[0] // grid_size[0], img_size[1] // grid_size[1])
+
+    anchors = [(a[0] / stride[0], a[1] / stride[1]) for a in anchors]
+
+    box_centers, box_sizes, confidence, classes = tf.split(predictions, [2, 2, 1, num_classes], axis=-1)
+
+    box_centers = np.maximum(box_centers, 0)
+    box_centers = np.minimum(box_centers, 1)
+
+    
+    grid_x = tf.range(grid_size[0], dtype=tf.float32)
+    grid_y = tf.range(grid_size[1], dtype=tf.float32)
+    a, b = tf.meshgrid(grid_x, grid_y)
+
+    x_offset = tf.reshape(a, (-1, 1))
+    y_offset = tf.reshape(b, (-1, 1))
+
+    x_y_offset = tf.concat([x_offset, y_offset], axis=-1)
+    x_y_offset = tf.reshape(tf.tile(x_y_offset, [1, num_anchors]), [1, -1, 2])
+
+    box_centers = box_centers + x_y_offset
+    box_centers = box_centers * stride
+
+    anchors = tf.tile(anchors, [dim, 1])
+    box_sizes = tf.exp(box_sizes) * anchors
+    box_sizes = box_sizes * stride
+
+    detections = tf.concat([box_centers, box_sizes, confidence], axis=-1)
+
+    predictions = tf.concat([detections, classes], axis=-1)
+    return predictions
